d680d8e86683560261754875a003661b292426c1
Kaden Scroggins <scroggik@nsuok.edu>
2022-12-08 11:36:12 -0600

    Refactored code and solved part 2

--- 2.py
+++ 2.py
@@ -1,32 +1,73 @@
 with open('input.txt') as f:
     lines = f.readlines()
 
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+LOSE = 0
+DRAW = 3
+WIN = 6
+
+'''
+Part 1
+'''
 score = 0
 
 for line in lines:
     if (line[2] == 'X'): # Rock
-        score += 1
+        score += ROCK
         if (line[0] == 'A'): # Rock, draw
-            score += 3
+            score += DRAW
         elif (line[0] == 'B'): # Paper, loss
-            score += 0
+            score += LOSE
         elif (line[0] == 'C'): # Scissors, win
-            score += 6
+            score += WIN
     elif (line[2] == 'Y'): # Paper
-        score += 2
+        score += PAPER
         if (line[0] == 'A'): # Rock, win
-            score += 6
+            score += WIN
         elif (line[0] == 'B'): # Paper, draw
-            score += 3
+            score += DRAW
         elif (line[0] == 'C'): # Scissors, loss
-            score += 0
+            score += LOSE
     elif (line[2] == 'Z'): # Scissors
-        score += 3
+        score += SCISSORS
         if (line[0] == 'A'): # Rock, loss
-            score += 0
+            score += LOSE
         elif (line[0] == 'B'): # Paper, win
-            score += 6
+            score += WIN
         elif (line[0] == 'C'): # Scissors, draw
-            score += 3
+            score += DRAW
 
-print('Score:', score)
+print('Part 1 score:', score)
+
+'''
+Part 2
+'''
+score = 0
+
+for line in lines:
+    if (line[0] == 'A'): # Rock
+        if (line[2] == 'X'): # Lose, Scissors
+            score += LOSE + SCISSORS
+        elif (line[2] == 'Y'): # Draw, Rock
+            score += DRAW + ROCK
+        elif (line[2] == 'Z'): # Win, Paper
+            score += WIN + PAPER
+    elif (line[0] == 'B'): # Paper
+        if (line[2] == 'X'): # Lose, Rock
+            score += LOSE + ROCK
+        elif (line[2] == 'Y'): # Draw, Paper
+            score += DRAW + PAPER
+        elif (line[2] == 'Z'): # Win, Scissors
+            score += WIN + SCISSORS
+    elif (line[0] == 'C'): # Scissors
+        if (line[2] == 'X'): # Lose, Paper
+            score += LOSE + PAPER
+        elif (line[2] == 'Y'): # Draw, Scissors
+            score += DRAW + SCISSORS
+        elif (line[2] == 'Z'): # Win, Rock
+            score += WIN + ROCK
+
+print('Part 2 score:', score)
